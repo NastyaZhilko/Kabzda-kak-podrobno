52db39959209cc462525adb2d55969202e680e85
NastyaZhilko <nasten4ik2010@gmail.com>
2021-06-01 18:33:28 +0300

    select-finish commit

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Select} from "./Select";
 import {action} from "@storybook/addon-actions";
 
@@ -8,22 +8,26 @@ export default {
     component: Select
 }
 
-export const WithValueExample = () =>
-
-        <Select onChange={action("Value changed")}
-                value={"2"}
+export const WithValueExample = () =>{
+const [value, setValue] = useState('2')
+    return (
+        <Select onChange={setValue}
+                value={value}
                 items={[
                     {value: "1", title: "Minsk"},
                     {value: "2", title: "Moscow"},
                     {value: "3", title: "Kiev"}
-                ]}/>;
+                ]}/>)
 
-
-export const WithoutValueExample = () =>
-
-        <Select onChange={action("Value changed")}
+}
+export const WithoutValueExample = () =>{
+    const [value, setValue] = useState(null)
+return(
+        <Select onChange={setValue}
+                value={value}
                 items={[
                     {value: "1", title: "Minsk"},
                     {value: "2", title: "Moscow"},
                     {value: "3", title: "Kiev"}
-                ]}/>;
+                ]}/>)
+}
