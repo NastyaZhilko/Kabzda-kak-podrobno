8e35fb1513854204a1c722371a81ccbc77350cac
NastyaZhilko <nasten4ik2010@gmail.com>
2021-06-01 18:46:42 +0300

    Updates

---
+++ precache-manifest.811aab0fc2aa04a996fd8378bffbb444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e861b8ff36c0a452ddfb8b3f7d8285e",
+    "url": "/Kabzda-kak-podrobno/index.html"
+  },
+  {
+    "revision": "6c06ae317455abcfad60",
+    "url": "/Kabzda-kak-podrobno/static/css/main.383e7b95.chunk.css"
+  },
+  {
+    "revision": "32f4c01f5f3cdb49b80f",
+    "url": "/Kabzda-kak-podrobno/static/js/2.ed45413c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Kabzda-kak-podrobno/static/js/2.ed45413c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c06ae317455abcfad60",
+    "url": "/Kabzda-kak-podrobno/static/js/main.a52f0161.chunk.js"
+  },
+  {
+    "revision": "ad500322669fc1586586",
+    "url": "/Kabzda-kak-podrobno/static/js/runtime-main.63f562e2.js"
+  }
+]);
